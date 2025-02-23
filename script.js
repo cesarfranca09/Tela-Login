a8fb1663b84c3ae346746104e147dd0f299fcc49
cesarfranca09 <cesarfranca.09@gmail.com>
2025-02-23 14:24:55 -0300

    Novo_Commit

--- script.js
+++ script.js
@@ -4,7 +4,7 @@ document.addEventListener('DOMContentLoaded', function() {
     loginForm.addEventListener('submit', function(event) {
         event.preventDefault(); // Vai impedir o envio padrão do formulário
 
-        // Obtenha os valores dos campos - validação básica
+       
         const username = document.getElementById('username').value.trim();
         const password = document.getElementById('password').value.trim();
 
@@ -13,11 +13,10 @@ document.addEventListener('DOMContentLoaded', function() {
             return; // Sai da função se algum campo estiver vazio
         }
 
-        // Simulação de envio 
+        
         console.log('Enviando dados:', { username, password });
         alert('Login realizado com sucesso!');
 
-        // Limpe os campos após o envio (opcional)
         loginForm.reset();
     });
 });
